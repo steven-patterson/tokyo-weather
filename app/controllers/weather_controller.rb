cf87fffb05b7ccb01682de452c0685f785c3fb23
bittercup <spatterson8@gmail.com>
2014-03-08 00:53:22 -0500

    Alternating sunset/sunrise icons

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,6 +3,15 @@ class WeatherController < ApplicationController
 		client = Weatherman::Client.new
 		response = client.lookup_by_woeid 1118370
 
+		Time.zone = "Tokyo"
+		@tokyo_time = Time.zone.now.strftime("%I:%M %p")
+		tokyo_time_ampm = Time.zone.now.strftime("%p")
+		if tokyo_time_ampm == "PM" && @tokyo_time.to_i != 12
+			@tokyo_time_24 = @tokyo_time.to_i + 12
+		else
+			@tokyo_time_24 = @tokyo_time.to_i
+		end
+
 		@city = response.location['city']
 		@country = response.location['country']
 
@@ -14,11 +23,14 @@ class WeatherController < ApplicationController
 		@sunrise = astronomy["sunrise"]
 		@sunset = astronomy["sunset"]
 
+		if @tokyo_time_24 >= @sunrise.to_i && @tokyo_time_24 < (@sunset.to_i+12)
+			@astro_icon = "sun-lower.svg"
+		else
+			@astro_icon = "moon.svg"
+		end
+
 		wind = response.wind
 		@wind_speed = wind["speed"]
-
-		Time.zone = "Tokyo"
-		@tokyo_time = Time.zone.now.strftime("%I:%M %p")
 
 	end
 end
