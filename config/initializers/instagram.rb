e0ff3206f033fce32c030dcf077a7520c8bcc50d
bittercup <spatterson8@gmail.com>
2014-03-08 03:14:11 -0500

    Instagram functionality added

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,9 +1,6 @@
 require "instagram"
 
  Instagram.configure do |config|
-
     config.client_id = "43dac718baa24bb8858c61440d3a3024"
-
-    config.access_token = "db86821b85b9481d85da06be98d68bcc"
-
+    config.access_token = "457496689.43dac71.33fde281859b473b9cba7bb9ffa2030b"
 end
